9a9dfec07a69914c8562aec91d88089852398c69
Cintia Abe <cintia.rossetto@gmail.com>
2022-12-05 16:40:11 -0300

    Resolução da Pergunta 1 - soma = 78

---
+++ src/testeTargetDesenvolvedorSP/pergunta1/ProgramPergunta1.java
@@ -0,0 +1,18 @@
+package testeTargetDesenvolvedorSP.pergunta1;
+
+public class ProgramPergunta1 {
+
+	public static void main(String[] args) {
+
+		int indice = 13;
+		int soma = 0;
+		int k = 0;
+		
+		for (int i=k; i<indice; i++) {
+			soma += i;
+		}
+		
+		System.out.println("A soma total dos números é : " + soma);
+	}
+
+}
